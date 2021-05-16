0eeae4de91ff80f119284ad2ebdc6efdf200f8e3
rishirajsahu <rishi.masters@gmail.com>
2021-05-16 21:24:24 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
 import Detail from './components/Detail'
+import Login from './components/Login'
 import { BrowserRouter as Router,
   Switch,
   Route,
@@ -17,6 +18,9 @@ function App() {
         <Switch>
           <Route path="/detail">
             <Detail/>
+          </Route>
+          <Route path="/login">
+            <Login/>
           </Route>
           <Route path="/">
             <Home/>
