f4818d45a3f4878dee940f5dc1959f1a0717abfd
rishirajsahu <rishi.masters@gmail.com>
2021-05-18 17:53:51 +0530

    ADD: grab movies from db

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Detail from './components/Detail'
 import Login from './components/Login'
 import { BrowserRouter as Router,
   Switch,
-  Route,
-  Link 
+  Route
 } from "react-router-dom";
 
 function App() {
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import { selectMovies } from '../features/movie/movieSlice'
+import { useSelector } from 'react-redux'
 
 function Movies() {
+
+    const movies = useSelector(selectMovies);
+    console.log(movies)
     return (
         <Container>
             <h4>Recommended For You</h4>
             <Content>
-                <Wrap>
-                    <img src="/images/slider-badging.jpg" />
-                </Wrap>
-                <Wrap>
-                    <img src="/images/slider-badging.jpg" />
-                </Wrap>
-                <Wrap>
-                    <img src="/images/slider-badging.jpg" />
-                </Wrap>
-                <Wrap>
-                    <img src="/images/slider-badging.jpg" />
-                </Wrap>
+                {movies && 
+                    movies.map((movie)=> (
+                    <Wrap key={movie.id}>
+                        <img src={movie.cardImg} />
+                    </Wrap>
+                    ))
+                }
             </Content>
         </Container>
     )
